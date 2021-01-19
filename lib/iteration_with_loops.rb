5cb81f3970bd1cbc3ea9c63567ccfb8b345b0f7b
yehudis raitman <yehudisr@gmail.com>
2021-01-19 00:30:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,7 +11,7 @@ def join_nested_strings(src)
 
     
     while element_index < src[row_index].count do
-    if src[row_index][element_index].is_a? == String
+    if src[row_index][element_index].class == String
       outer_results << src[row_index][element_index]  
      end 
      element_index += 1 
